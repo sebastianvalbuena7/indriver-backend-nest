29c27aae14edc39c4deb1eec6a8216544c16edad
Sebastian <sebastianvalbuena25@gmail.com>
2025-03-05 20:42:57 -0500

    Rama main

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
-// * security
+// * main
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') { }
